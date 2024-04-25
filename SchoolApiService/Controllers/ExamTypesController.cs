3777a02294859d33e15c0d80de0c0d66139c3e0a
DummyCollaborator <collaborator620@gmail.com>
2024-04-25 15:25:24 +0600

    Added Login functionality

--- SchoolApiService/Controllers/ExamTypesController.cs
+++ SchoolApiService/Controllers/ExamTypesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ namespace SchoolApiService.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ExamTypesController : ControllerBase
     {
         private readonly SchoolDbContext _context;
--- SchoolAppClient.NG/src/app/Authentication/SecurityComponents/login/login.component.ts
+++ SchoolAppClient.NG/src/app/Authentication/SecurityComponents/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, ViewChild, inject, viewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 //import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { AuthRequest } from '../../SecurityModels/auth-request';
@@ -15,7 +15,7 @@ import { AuthService } from '../../SecurityModels/auth.service';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  @ViewChild('loginForm') loginForm!: NgForm;
   model!: AuthRequest;
   authService = inject(AuthService);
   router = inject(Router);
@@ -23,6 +23,14 @@ export class LoginComponent {
     this.model = new AuthRequest();
   }
   login(event: Event) {
+
+    if (this.loginForm.invalid) {
+      this.loginForm.form.markAllAsTouched();
+      return;
+    }
+
+
+
     event.preventDefault();
 
     console.log(`Login: ${this.model.email} / ${this.model.password}`);
